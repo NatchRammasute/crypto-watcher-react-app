58c245ede2e75d6c8d8623bb5fdaf600380cc77e
Natchapong Rammasute <natchapongrammasute@Natchapongs-MacBook-Pro.local>
2021-11-17 21:09:36 +0700

    adding basic layout with ant design and create router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import './App.css'
+import { Navbar, Exchange, Homepage, Cryptocurrencies, News, CryptoDetail  } from './components'
+import { whileStatement } from '@babel/types'
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="app">
+            <div className="navbar">
+                < Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchange">
+                                <Exchange />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetail />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+                <div className="footer"  >
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Crptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchange">Exchange</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/CryptoDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetail = () => {
+    return (
+        <div>
+            <h1>CryptoDeatil</h1>
+        </div>
+    )
+}
+
+export default CryptoDetail
